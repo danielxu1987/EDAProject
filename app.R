725c1d21271180aa54a53c352aee7830a24c63cb
Daniel Xu <sxu.cbs@gmail.com>
2024-09-21 16:36:56 +0800

    Single variable for histogram also works

--- app.R
+++ app.R
@@ -11,38 +11,86 @@ library(knitr)
 # load dataset
 df <- read.csv("./Countries and death causes.csv", header = T, sep=",")
 
+
+### Single histogram
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("EDA for single variable"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          selectInput(inputId = "con", label = "Choose country", choices = unique(df$Entity)),
-          selectInput(inputId = "var", label = "Choose death cause", choices = names(df[, -c(1:3)]))
-        ),
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("histogram")
-        )
+  
+  # Application title
+  titlePanel("Histgram for single variable"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      # selectInput(inputId = "con", label = "Choose country", choices = unique(df$Entity)),
+      selectInput(inputId = "x", label = "Choose death cause", choices = names(df[, -c(1:3)])),
+      # selectInput(inputId = "var2", label = "Choose death cause2", choices = names(df[, -c(1:3)]))
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("histogram")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    generate_histogram <- function(data, con, x_var, title) {
-      data <- data[data$Entity == con,]
-      ggplot(data, aes_string(x = x_var)) +
-        geom_histogram(bins = 20, fill = "gray") +
-        labs(title = title, x = x_var, y = "Frequency", subtitle = 'Frequency per cause per country')
-    }
-    # Render the side-by-side histograms
-    output$histogram <- renderPlot({
-      generate_histogram(df, input$con, input$var, colnames(input$var))
-    })
+  generate_histogram <- function(data, var, title) {
+    ggplot(data) +
+      geom_histogram(aes_string(x = var), bins = 40, fill = "gray") +
+      labs(title = title, x = var, y = "Frequency", subtitle = 'Frequency per cause for all countries')
+  }
+  # Render the side-by-side histograms
+  output$histogram <- renderPlot({
+    generate_histogram(df, input$x, colnames(input$var))
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+# ## Two variables histogram
+# # Define UI for application that draws a histogram
+# ui <- fluidPage(
+#   
+#   # Application title
+#   titlePanel("EDA for single variable"),
+#   
+#   # Sidebar with a slider input for number of bins 
+#   sidebarLayout(
+#     sidebarPanel(
+#       selectInput(inputId = "con", label = "Choose country", choices = unique(df$Entity)),
+#       selectInput(inputId = "var1", label = "Choose death cause1", choices = names(df[, -c(1:3)])),
+#       selectInput(inputId = "var2", label = "Choose death cause2", choices = names(df[, -c(1:3)]))
+#     ),
+#     # Show a plot of the generated distribution
+#     mainPanel(
+#       plotOutput("histogram")
+#     )
+#   )
+# )
+# 
+# # Define server logic required to draw a histogram
+# server <- function(input, output) {
+#   generate_histogram <- function(data, con, x_var1, x_var2, title) {
+#     data <- data[data$Entity == con,]
+#     
+#     p1 <- ggplot(data, aes_string(x = x_var1)) +
+#       geom_histogram(bins = 20, fill = "gray") +
+#       labs(title = title, x = x_var1, y = "Frequency", subtitle = 'Frequency per cause per country')
+#     
+#     p2 <- ggplot(data, aes_string(x = x_var2)) +
+#       geom_histogram(bins = 20, fill = "gray") +
+#       labs(title = title, x = x_var2, y = "Frequency", subtitle = 'Frequency per cause per country')
+#     
+#     grid.arrange(p1, p2, nrow = 1)
+#   }
+#   # Render the side-by-side histograms
+#   output$histogram <- renderPlot({
+#     generate_histogram(df, input$con, input$var1, input$var2, colnames(input$var))
+#   })
+# }
+# 
+# # Run the application 
+# shinyApp(ui = ui, server = server)
