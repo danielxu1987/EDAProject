24f125ef221c1540cc07345d53e5d54cfaf6e860
Daniel Xu <sxu.cbs@gmail.com>
2024-09-21 21:23:29 +0800

    Simple boxplot is added per country per variable.

--- app.R
+++ app.R
@@ -14,36 +14,68 @@ df <- read.csv("./Countries and death causes.csv", header = T, sep=",")
 
 ### Single histogram
 
+# # Define UI for application that draws a histogram
+# ui <- fluidPage(
+#   
+#   # Application title
+#   titlePanel("Histgram for single variable"),
+#   
+#   # Sidebar with a slider input for number of bins 
+#   sidebarLayout(
+#     sidebarPanel(
+#       # selectInput(inputId = "con", label = "Choose country", choices = unique(df$Entity)),
+#       selectInput(inputId = "x", label = "Choose death cause", choices = names(df[, -c(1:3)])),
+#       # selectInput(inputId = "var2", label = "Choose death cause2", choices = names(df[, -c(1:3)]))
+#     ),
+#     # Show a plot of the generated distribution
+#     mainPanel(
+#       plotOutput("histogram")
+#     )
+#   )
+# )
+# 
+# # Define server logic required to draw a histogram
+# server <- function(input, output) {
+#   generate_histogram <- function(data, var, title) {
+#     ggplot(data) +
+#       geom_histogram(aes_string(x = var), bins = 40, fill = "gray") +
+#       labs(title = title, x = var, y = "Frequency", subtitle = 'Frequency per cause for all countries')
+#   }
+#   # Render the side-by-side histograms
+#   output$histogram <- renderPlot({
+#     generate_histogram(df, input$x, colnames(input$var))
+#   })
+# }
+
+# # Run the application 
+# shinyApp(ui = ui, server = server)
+
+##Single boxplot
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Histgram for single variable"),
+  titlePanel("Boxplot for single death cause per country"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      # selectInput(inputId = "con", label = "Choose country", choices = unique(df$Entity)),
-      selectInput(inputId = "x", label = "Choose death cause", choices = names(df[, -c(1:3)])),
-      # selectInput(inputId = "var2", label = "Choose death cause2", choices = names(df[, -c(1:3)]))
+      selectInput(inputId = "con", label = "Choose country", choices = unique(df$Entity)),
+      selectInput(inputId = "var", label = "Choose death cause", choices = names(df[, -c(1:3)])),
     ),
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("histogram")
+      plotOutput("boxplot")
     )
   )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  generate_histogram <- function(data, var, title) {
-    ggplot(data) +
-      geom_histogram(aes_string(x = var), bins = 40, fill = "gray") +
-      labs(title = title, x = var, y = "Frequency", subtitle = 'Frequency per cause for all countries')
-  }
   # Render the side-by-side histograms
-  output$histogram <- renderPlot({
-    generate_histogram(df, input$x, colnames(input$var))
+  output$boxplot <- renderPlot({
+    data <- df[df['Entity']==input$con,] #df[df['Entity']=='China',]
+    boxplot(data[input$var], xlab=input$var, ylab = 'Count')
   })
 }
 
